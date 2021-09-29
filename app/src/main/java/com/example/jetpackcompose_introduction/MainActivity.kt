37f1d1b656a368ba380d5dfe913ea61987653765
Pankaj <pankaj200635@gmail.com>
2021-09-29 16:10:47 +0530

    Layout Basics with Image and Text

--- app/src/main/java/com/example/jetpackcompose_introduction/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose_introduction/MainActivity.kt
@@ -4,19 +4,28 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.fontResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcompose_introduction.ui.theme.JetpackCompose_IntroductionTheme
 
@@ -24,26 +33,34 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Title()
+            UserCard()
         }
 
     }
 }
 
 @Composable
-fun Title() {
-val context = LocalContext.current
-    Text(
-        text = "Simplified Coding",
-        fontSize = 32.sp,
-        fontFamily = FontFamily.Cursive,
-        color = colorResource(id = R.color.purple_200),
-        modifier =  Modifier.clickable {
-            Toast.makeText(context,"Title Clicked",Toast.LENGTH_LONG).show()
+fun UserCard() {
+    Row (
+        modifier = Modifier.fillMaxWidth().wrapContentHeight()
+            .padding(12.dp)
+            .border(1.dp,color = Color.DarkGray)
+            .padding(12.dp)//we don't have margin in jetpack compose
+            ){        Image(
+            painter = painterResource(id = android.R.drawable.btn_star_big_on),
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(160.dp)
+                .clip(CircleShape)
+        )
+        Column {
+            Text(text = stringResource(id = R.string.dummy_text))
+            Button(onClick = { /*TODO*/ }) {
+               Text(text = "View Profile")
+            }
         }
-    )
-
-
+    }
 }
 
 @Preview(showBackground = true)
@@ -54,6 +71,6 @@ fun DefaultPreview() {
             .fillMaxHeight()
             .fillMaxWidth()
     ) {
-        Title()
+        UserCard()
     }
 }
