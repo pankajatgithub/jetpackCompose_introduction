0bf098daf25ca51df3b2833303b8a32bc23544e9
Pankaj <pankaj200635@gmail.com>
2021-09-29 17:32:59 +0530

    RecyclerView and CardView

--- app/src/main/java/com/example/jetpackcompose_introduction/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose_introduction/MainActivity.kt
@@ -4,15 +4,12 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,31 +30,73 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            UserCard()
+            UserList()
         }
 
     }
 }
 
+data class User(
+    val id : Int
+)
+val users = listOf(
+    User(1),
+    User(1),
+    User(1),
+    User(1),
+    User(1),
+    User(1),
+    User(1),
+    User(1)
+)
+@Composable
+fun UserList(){
+//    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+//        for(i in 1..10)
+//        {
+//            UserCard()
+//        }
+//    }
+    LazyColumn{
+        items(users){user ->
+            UserCard()
+
+        }
+    }
+
+}
+
 @Composable
 fun UserCard() {
-    Row (
-        modifier = Modifier.fillMaxWidth().wrapContentHeight()
+    Card(
+        elevation = 4.dp,
+        modifier = Modifier
             .padding(12.dp)
-            .border(1.dp,color = Color.DarkGray)
-            .padding(12.dp)//we don't have margin in jetpack compose
-            ){        Image(
-            painter = painterResource(id = android.R.drawable.btn_star_big_on),
-            contentDescription = "",
-            contentScale = ContentScale.Crop,
+            .fillMaxWidth()
+            .wrapContentHeight()
+    ) {
+
+        Row(
             modifier = Modifier
-                .size(160.dp)
-                .clip(CircleShape)
-        )
-        Column {
-            Text(text = stringResource(id = R.string.dummy_text))
-            Button(onClick = { /*TODO*/ }) {
-               Text(text = "View Profile")
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(12.dp)
+                .border(1.dp, color = Color.DarkGray)
+                .padding(12.dp)//we don't have margin in jetpack compose
+        ) {
+            Image(
+                painter = painterResource(id = android.R.drawable.btn_star_big_on),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(130.dp)
+                    .clip(CircleShape)
+            )
+            Column {
+                Text(text = stringResource(id = R.string.dummy_text))
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "View Profile")
+                }
             }
         }
     }
@@ -71,6 +110,6 @@ fun DefaultPreview() {
             .fillMaxHeight()
             .fillMaxWidth()
     ) {
-        UserCard()
+        UserList()
     }
 }
